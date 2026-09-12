74308982b8b3d7f82d5d53dd7d03b3865ceea5b4
Franco Villanueva Zeballos <fredyvillanueva114@gmail.com>
2026-09-12 09:21:31 -0500

    BUBBLE SORT - MEJORA 1

--- main.cpp/main.cpp
+++ main.cpp/main.cpp
@@ -2,7 +2,6 @@
 
 using namespace std;
 
-
 // BUBBLE SORT BASE
 void bubbleSort(int A[], int n, int &comparaciones, int &intercambios) {
 
@@ -27,26 +26,71 @@ void bubbleSort(int A[], int n, int &comparaciones, int &intercambios) {
     }
 }
 
+// BUBBLE SORT - MEJORA 1
+// Detecta si el arreglo ya está ordenado
+void bubbleSortMejora1(int A[], int n, int &comparaciones, int &intercambios) {
+
+    comparaciones = 0;
+    intercambios = 0;
+
+    for (int i = 0; i < n - 1; i++) {
+
+        bool swapped = false;
+
+        for (int j = 0; j < n - 1; j++) {
+
+            comparaciones++;
+
+            if (A[j] > A[j + 1]) {
+
+                int aux = A[j];
+                A[j] = A[j + 1];
+                A[j + 1] = aux;
+
+                intercambios++;
+
+                swapped = true;
+            }
+        }
+
+        if (!swapped) {
+            break;
+        }
+    }
+}
 
 // MAIN PARA PROBAR BUBBLE SORT
 int main() {
 
-    int arreglo[] = {5, 3, 8, 2};
     int n = 4;
 
-    int comparaciones;
-    int intercambios;
+    int arregloBase[] = {5, 3, 8, 2};
+    int arregloMejora[] = {5, 3, 8, 2};
 
-    bubbleSort(arreglo, n, comparaciones, intercambios);
+    int comparaciones, intercambios;
 
+
+    bubbleSort(arregloBase, n, comparaciones, intercambios);
+
+    cout << "=== BUBBLE SORT BASE ===" << endl;
     cout << "Arreglo ordenado: ";
-
     for (int i = 0; i < n; i++) {
-        cout << arreglo[i] << " ";
+        cout << arregloBase[i] << " ";
     }
-
     cout << endl;
+    cout << "Comparaciones: " << comparaciones << endl;
+    cout << "Intercambios: " << intercambios << endl << endl;
 
+
+
+    bubbleSortMejora1(arregloMejora, n, comparaciones, intercambios);
+
+    cout << "=== BUBBLE SORT MEJORA 1 ===" << endl;
+    cout << "Arreglo ordenado: ";
+    for (int i = 0; i < n; i++) {
+        cout << arregloMejora[i] << " ";
+    }
+    cout << endl;
     cout << "Comparaciones: " << comparaciones << endl;
     cout << "Intercambios: " << intercambios << endl;
 
